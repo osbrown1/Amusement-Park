7396e32b11c9c6a2ec772cd40b13ce7bef4615ff
osbrown1 <socknot@live.com>
2023-01-11 17:02:48 -0800

    for reference

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// User Interface Logic
+
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("swings").setAttribute("class", "hidden");
+  document.getElementById("coaster").setAttribute("class", "hidden");
+  document.getElementById("tower").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+}
+
+window.onload = function() {
+  hideResultsAndError();
+
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    const age = parseInt(document.querySelector("input#age").value);
+    const height = parseInt(document.querySelector("input#height").value);
+
+    if (age && height) {
+      if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+      } else if (age >= 12 || height >= 48) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+      } else if (age >= 6) {
+        document.getElementById("swings").removeAttribute("class");
+      } else {
+        document.getElementById("sorry").removeAttribute("class");
+      }
+    } else {
+      document.getElementById("error-message").removeAttribute("class");
+    }
+  };
+};
